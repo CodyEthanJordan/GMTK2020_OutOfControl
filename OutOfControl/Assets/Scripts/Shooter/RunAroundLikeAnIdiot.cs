1f37bc1e5209cebd5b11c2993246dbd22d847700
CodyEthanJordan <codyethanjordan@gmail.com>
2020-07-10 19:58:25 -0400

    Fixing player and powerup movement

--- OutOfControl/Assets/Scripts/Shooter/RunAroundLikeAnIdiot.cs
+++ OutOfControl/Assets/Scripts/Shooter/RunAroundLikeAnIdiot.cs
@@ -11,10 +11,12 @@ namespace Assets.Scripts.Shooter
     {
         public float Speed = 10;
         public float JumpPower = 10;
-        public float JumpTimer = 3;
+        public float JumpTime = 3;
+        public float TurnaroundTime = 3;
         public int Direction = 1;
 
-        private float timer = 0;
+        private float jumpTimer = 0;
+        private float turnTimer;
         private Rigidbody2D rb;
 
         private void Start()
@@ -26,11 +28,18 @@ namespace Assets.Scripts.Shooter
         {
             rb.velocity = new Vector2(Speed * Direction, rb.velocity.y);
 
-            timer -= Time.fixedDeltaTime;
-            if(timer <= 0)
+            jumpTimer -= Time.fixedDeltaTime;
+            if(jumpTimer <= 0)
             {
                 rb.AddForce(Vector2.up * JumpPower, ForceMode2D.Impulse);
-                timer = UnityEngine.Random.Range(JumpTimer / 2, 2 * JumpTimer);
+                jumpTimer = UnityEngine.Random.Range(JumpTime / 2, 2 * JumpTime);
+            }
+
+            turnTimer -= Time.fixedDeltaTime;
+            if(turnTimer <= 0)
+            {
+                Direction *= -1;
+                turnTimer = TurnaroundTime;
             }
         }
 
