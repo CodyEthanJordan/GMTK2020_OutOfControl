996775622f3482cd4797f6a0137b4e67d7dc03bb
CodyEthanJordan <codyethanjordan@users.noreply.github.com>
2020-07-10 16:46:13 -0400

    Adding pico 8 tiles

---
+++ OutOfControl/Assets/Scripts/RunnerGuy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class RunnerGuy : MonoBehaviour
+    {
+        private Rigidbody2D rb;
+
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+    }
+}
